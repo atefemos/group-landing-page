30c567a133d7e615810ebf926a4b9e9bc1fdfe36
a.moslemi.68@gmail.com <a.moslemi.68@gmail.com>
2025-04-21 21:01:54 +0200

    fix: images and styles

--- src/pages/mainPage.tsx
+++ src/pages/mainPage.tsx
@@ -1,8 +1,10 @@
 import React, { useEffect } from "react"
-import { useDispatch } from "react-redux"
-import { useNavigate } from "react-router-dom"
-import { getPostsStore } from "../store/slices/posts.ts"
-import { createDataTree } from "../utils/index.ts"
+import Person1 from "../assets/images/person1.jpg"
+import Person2 from "../assets/images/person2.webp"
+import Person3 from "../assets/images/person3.jpeg"
+import Person4 from "../assets/images/person4.webp"
+import Person5 from "../assets/images/person5.avif"
+import Person6 from "../assets/images/person6.jpg"
 import { IoArrowForwardOutline } from "react-icons/io5"
 
 const MainPage: React.FC = () => {
@@ -10,8 +12,8 @@ const MainPage: React.FC = () => {
 
   return (
     <div>
-      <section className='flex flex-col lg:flex-row place-items-center gap-[50px] overflow-hidden py-11 px-8 lg:px-[80px] lg:py-[130px] bg-[radial-gradient(circle,_rgba(67,_69,_112,_1)_3%,_rgba(35,_36,_57,_1)_60%)]'>
-        <div className="text-white px-0 lg:px-[50px] py-0 lg:w-1/2 max-w-[450px]">  
+      <section className='flex flex-col-reverse md:flex-row place-items-center overflow-hidden py-11 px-8 md:px-[80px] md:py-[130px] bg-[radial-gradient(circle,_rgba(67,_69,_112,_1)_3%,_rgba(35,_36,_57,_1)_60%)]'>
+        <div className="text-white px-0 md:px-[50px] py-0 md:w-1/2">  
         <h1 className="font-title title-fluid mb-8">
             <div className="font-title bg-[linear-gradient(90deg,_rgb(118,167,63)_0%,_rgb(51,143,118)_40%,_rgb(55,141,167)_50%,_rgb(117,152,242)_70%,_rgb(144,118,236)_100%)] bg-clip-text text-transparent [background-size:contain] [-webkit-background-clip:text]">Empowering your business </div>
             with smart tech
@@ -27,7 +29,7 @@ const MainPage: React.FC = () => {
             aria-label="email adress"
             placeholder=""
             required
-            className="px-[10px] bg-transparent outline-transparent border-0 border-b-2 border-white/30 mr-[10px] transition-all duration-300 ease-in placeholder:text-[#7598f2] placeholder:opacity-50 placeholder:font-medium"
+            className="px-[10px] w-full bg-transparent outline-transparent border-0 border-b-2 border-white/30 mr-[10px] transition-all duration-300 ease-in placeholder:text-[#7598f2] placeholder:opacity-50 placeholder:font-medium"
             onInput="checkEmpty()" />
           <button type="submit" className="btn" aria-label="submit">
             <span>Subscribe</span>
@@ -35,13 +37,13 @@ const MainPage: React.FC = () => {
             </button>
           </form>
         </div>
-        <div className="grid grid-cols-4 grid-rows-3 gap-5 lg:w-1/2">
+        <div className="grid grid-cols-4 grid-rows-3 gap-5 mb-10 md:w-1/2">
           <span className="item"
             style={{ "--i": 1 } as React.CSSProperties}
           ></span>
         <img
           className="item"
-          src="https://github.com/ecemgo/mini-samples-great-tricks/assets/13468728/274f29ce-0d3f-4ac2-a2aa-f9b7bd188b2a"
+            src={Person1}
           style={{ "--i": 2 } as React.CSSProperties}
           alt="" />
           <span className="item"
@@ -49,19 +51,19 @@ const MainPage: React.FC = () => {
           ></span>
         <img
           className="item"
-          src="https://github.com/ecemgo/mini-samples-great-tricks/assets/13468728/b8a14493-3d9f-4b9b-b93a-56d0bc7243e9"
+          src={Person2}
           style={{ "--i": 4 } as React.CSSProperties}
           alt="" />
 
         <img
           className="item"
-          src="https://github.com/ecemgo/mini-samples-great-tricks/assets/13468728/03e51e1e-9750-45a5-b75e-a1e341d4562a"
+            src={Person3}
           style={{ "--i": 10 } as React.CSSProperties}
           alt="" />
           <span className="item"
             style={{ "--i": 11 } as React.CSSProperties}
           ></span>
-          <img className="item" src="https://github.com/ecemgo/mini-samples-great-tricks/assets/13468728/5eb50f89-3e5a-480e-860c-8d40d3ba9ffe"
+          <img className="item" src={Person4}
             style={{ "--i": 12 } as React.CSSProperties}
             alt="" />
           <span className="item"
@@ -71,14 +73,12 @@ const MainPage: React.FC = () => {
           <span className="item"
             style={{ "--i": 9 } as React.CSSProperties}
           ></span>
-          <img className="item" src="https://github.com/ecemgo/mini-samples-great-tricks/assets/13468728/86c71a79-2efe-4567-8665-b1e5a1fd9735"
-            style={{ "--i": 8 } as React.CSSProperties}
+          <img className="item" src={Person5}
             alt="" />
           <span className="item"
             style={{ "--i": 7 } as React.CSSProperties}
           ></span>
-          <img className="item" src="https://github.com/ecemgo/mini-samples-great-tricks/assets/13468728/97ef9643-5202-41aa-80f0-ceeabccdd099"
-            style={{ "--i": 6 } as React.CSSProperties}
+          <img className="item" src={Person6}
             alt="" />
       </div>
       </section>
